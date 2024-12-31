2f26ef5263b0825726bc480cffda54d1d5057d4b
Preetibansal08 <bansalpreeti805@gmail.com>
2024-12-31 23:43:47 +0530

    first commit

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,76 @@
+import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
+import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
+
+const portfolioProjects = [
+  {
+    company: "Acme Corp",
+    year: "2022",
+    title: "Dark Saas Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/4k7IdSLxh6w",
+    image: darkSaasLandingPage,
+  },
+  {
+    company: "Innovative Co",
+    year: "2021",
+    title: "Light Saas Landing Page",
+    results: [
+      { title: "Boosted sales by 20%" },
+      { title: "Expanded customer reach by 35%" },
+      { title: "Increased brand awareness by 15%" },
+    ],
+    link: "https://youtu.be/7hi5zwO75yc",
+    image: lightSaasLandingPage,
+  },
+  {
+    company: "Quantum Dynamics",
+    year: "2023",
+    title: "AI Startup Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/Z7I5uSRHMHg",
+    image: aiStartupLandingPage,
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+    <div>
+      <div className="container">
+        <p className="upper">Real-World Impact</p>
+        <h2>Discover My Creations</h2>
+        <p>Every project is a story of learning, growth, and exploration. These experiences have shaped me as a developer and problem-solver.</p>
+        <div>
+          {portfolioProjects.map(project => (
+            <div key={project.title}>
+              <div>
+                <span>{project.company}</span>
+                <span>{project.year}</span>
+              </div>
+              <h3>{project.title}</h3>
+              <hr />
+              <ul>
+                {project.results.map(result => (
+                  <li>{result.title}</li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button>View Project</button>
+              </a>
+              <Image src={project.image} alt={project.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
